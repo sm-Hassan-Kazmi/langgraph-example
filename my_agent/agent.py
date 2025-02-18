d41c28bd07c6c67e76afcf3bb8e836a177f2f480
sm-Hassan-Kazmi <smhk760@gmail.com>
2025-02-18 10:46:38 +0500

    comment code for running on langsmith

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -51,23 +51,23 @@ workflow.add_edge("action", "agent")
 # meaning you can use it as you would any other runnable
 graph = workflow.compile()
 
-def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [("user", user_input)]}):
-        for value in event.values():
-            print(value)
-            print("Assistant:", value["messages"][-1].content)
+# def stream_graph_updates(user_input: str):
+#     for event in graph.stream({"messages": [("user", user_input)]}):
+#         for value in event.values():
+#             print(value)
+#             print("Assistant:", value["messages"][-1].content)
 
-while True:
-    try:
-        user_input = input("User: ")
-        if user_input.lower() in ["quit", "exit", "q"]:
-            print("Goodbye!")
-            break
+# while True:
+#     try:
+#         user_input = input("User: ")
+#         if user_input.lower() in ["quit", "exit", "q"]:
+#             print("Goodbye!")
+#             break
 
-        stream_graph_updates(user_input)
-    except:
-        # fallback if input() is not available
-        user_input = "What do you know about LangGraph?"
-        print("User: " + user_input)
-        stream_graph_updates(user_input)
-        break
+#         stream_graph_updates(user_input)
+#     except:
+#         # fallback if input() is not available
+#         user_input = "What do you know about LangGraph?"
+#         print("User: " + user_input)
+#         stream_graph_updates(user_input)
+#         break
